207fe2bbee6a72759507663ab59bc9aa0418c0df
Steve Dunham <dunhamsteve@gmail.com>
2023-01-25 22:13:38 -0800

    add proofs to Heap.lean, fix bug

--- Aoc2022/Heap.lean
+++ Aoc2022/Heap.lean
@@ -1,4 +1,8 @@
-
+/-
+This file contains an exercise, implement a max heap in lean. It uses a zero-based
+array, which makes some of the math (and proofs) trickier. But I think I've managed
+to get it right and prove termination + indices.
+-/
 
 structure Heap (α) (lt : α -> α -> Bool) where
   data : Array α
@@ -7,15 +11,24 @@ deriving Repr
 private
 def sink (a : Array α) (lt : α -> α -> Bool) (ix : Nat): Array α :=
   if h : 0 < ix then
-    -- I thought it'd be able to search for this stuff.
-    have : ix / 2 < ix := Nat.div_lt_self h (1).lt_succ_self
+    let ix' := (ix - 1)/2
+    have : ix' < ix := by
+      cases h1 : ix - 1
+      . apply Nat.lt_of_le_of_lt (Nat.div_le_self (ix - 1) 2)
+        rw [h1]
+        assumption
+      . apply Nat.lt_of_lt_of_le (Nat.div_lt_self _ (1).lt_succ_self)
+        apply Nat.sub_le
+        rw [h1]
+        apply Nat.zero_lt_succ
+
     if h : ix < a.size then
-      have : ix/2 < a.size := Nat.lt_trans this h
-      if lt a[ix/2] a[ix] then
+      have : ix' < a.size := Nat.lt_trans this h
+      if lt a[ix'] a[ix] then
         let x := a[ix]
-        let a := (a.setD ix a[ix/2]).setD (ix/2) x
-        sink a lt (ix/2)
-      -- is there an elegant way to not have all of these?
+        let a := (a.set ⟨ ix, h ⟩  a[ix']).set ⟨ ix', by simp [this] ⟩ x
+        sink a lt ix'
+      -- is there an elegant way to not have all of these elses?
       -- I couldn't and the if's up there because I needed dependent if
       else a
     else a
@@ -28,35 +41,52 @@ def Heap.insert (h : Heap α lt) (a : α) : Heap α lt where
 
 def Heap.max (h : Heap α lt) : Option α := h.data[0]?
 
--- TODO terminates because ix is increasing and less than arr.size, but 
--- I don't know enough lean to show that.
 private
-partial
-def bubble (arr : Array α) (lt : α -> α -> Bool) (ix : Nat) : Array α :=
-  if h : ix < arr.size then
-    let j := 2 * ix
-    let k := 2 * ix + 1
-    let a := arr[ix]
-    if h : k < arr.size then
-      -- is there a better way to write this?
-      have : j < arr.size := Nat.lt_trans (2*ix).lt_succ_self h
-      let b := arr[j]
-      let c := arr[k]
-      if lt b c && lt a c then
-        -- bubble k side
-        bubble ((arr.setD k a).setD ix c) lt k
-      else if lt a b then
-        bubble ((arr.setD j a).setD ix b) lt j
-      else
-        arr
-    else if h : j < arr.size then
-      let b := arr[j]
-      if lt a b then
-          bubble ((arr.setD j a).setD ix b) lt j
+def bubble (arr : Array α) (lt : α -> α -> Bool) (ix : Nat) : Array α :=  
+  let rec go (arr : Array α) (ix : Nat) := 
+    -- We can probably pass this constraint in.
+    if h1 : ix < arr.size then
+      have ha : arr.size - 2*ix <= arr.size - ix := by 
+              simp [Nat.sub_add_eq,Nat.sub_le,Nat.succ_mul]
+              
+      let j := 2 * ix + 1
+      let k := 2 * ix + 2
+      let a := arr[ix]
+      if h : k < arr.size then
+        -- is there a better way to write this?
+        have h2 : j < arr.size := Nat.lt_trans (2*ix + 1).lt_succ_self h
+        have h3 : 2*ix < arr.size := Nat.lt_trans (2*ix).lt_succ_self h2
+        let b := arr[j]
+        let c := arr[k]
+        if lt b c && lt a c then -- bubble k side          
+          have : arr.size - k < arr.size - ix := by 
+            apply Nat.lt_trans (Nat.sub_succ_lt_self arr.size (2*ix+1) h2)
+            apply Nat.lt_of_lt_of_le (Nat.sub_succ_lt_self arr.size (2*ix) h3)
+            exact ha
+
+          go ((arr.set ⟨k,h⟩ a).set ⟨ix, by simp [h1]⟩ c) k
+        else if lt a b then
+          have : arr.size - j < arr.size - ix := by 
+            apply Nat.lt_of_lt_of_le (Nat.sub_succ_lt_self arr.size (2*ix) h3)
+            exact ha
+          go ((arr.set ⟨ j, h2 ⟩  a).set ⟨ ix, by simp [h1] ⟩ b) j
+        else
+          arr
+      else if h : j < arr.size then
+        let b := arr[j]
+        if lt a b then
+            have : arr.size - j < arr.size - ix := by 
+              have h3 : 2*ix < arr.size := Nat.lt_trans (2*ix).lt_succ_self h
+              apply Nat.lt_of_lt_of_le (Nat.sub_succ_lt_self arr.size (2*ix) h3)
+              exact ha
+
+            go ((arr.set ⟨ j, h ⟩  a).set ⟨ ix, by simp [h1] ⟩  b) j
+        else arr
       else arr
     else arr
-  else arr
--- termination_by _ arr ix => arr.size - ix
+  go arr ix
+termination_by go arr ix => arr.size - ix
+
 
 private
 def popAux (arr : Array α) (lt : α -> α -> Bool) : Array α :=
@@ -83,7 +113,7 @@ def ltNat (a b : Nat) : Bool := a < b
 private
 partial
 def main (argv : List String) : IO Unit := do
-  let x := [1,234,1,35,6,8,0,10,11,2,3,8,6,4,3,0,1,0,1,0,1]
+  let x := [1,400,8,7,6,5,6,5,7,8,9,234,1,35,6,8,0,10,11,2,3,8,6,4,3,0,1,0,1,0,1,500,1024]
   let h := makeHeap ltNat
   let h := x.foldl (Heap.insert) h
   println! "{h.data}"
@@ -92,7 +122,7 @@ def main (argv : List String) : IO Unit := do
     match h.max with
     | .none => pure ()
     | .some x => do
-        println! "{h.data}"
+        -- println! "{h.data}"
         println! "{x}"
         loop h.pop
   loop h
